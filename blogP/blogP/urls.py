778cd7f2cda46bb919ca353dc123ebdc8a2ccab0
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-02-05 18:32:36 +1100

    Bug-fixes in blogp/urls.py

--- blogP/blogP/urls.py
+++ blogP/blogP/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from boards import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('boards/<int:pk>/', views.board_topics, name='board_topics'),
+    path('admin/', admin.site.urls),
 ]
